f736c557d73dc5bd03ed0f234fa03e6949dfa2f1
Abu Taher <abutahercse3255@gmail.com>
2024-09-24 12:27:50 +0600

    final

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name="Donal Trump";
+        $age="75";
+        $data=[
+            'id'=>$id,
+            'name'=>$name,
+            'age'=>$age
+        ];
+
+        $cookie_name='access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+        return response()->json($data, 200)
+            ->cookie($cookie_name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+    }
+}
